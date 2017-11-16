9d33d86634f3f73d1a01556a6dda32cd7970c15c
G-Wayne <32604878+G-Wayne@users.noreply.github.com>
2017-11-16 16:54:39 -0400

    This is Exercise 4 plus added CSS

--- world.js
+++ world.js
@@ -2,18 +2,38 @@
 $(document).ready(function(){
 
     $("#lookup").on('click', function(){
-
+        if($("#check").is(":checked")){
+           searchAll();
+        }
+        else if ($("#country").val() != ""){
             search($("#country").val());
+        }
+        else{
+            $("#result").html("Invalid Input ! Please enter a valid country");
+        }
+
 
     });
 });
 
 function search(country){
-    var link = "https://info2180-lab7-glawton.c9users.io/world.php?country=" + country;
-    $.ajax(link,{
+    var url = "https://info2180-lab7-glawton.c9users.io/world.php?country=" + country;
+    $.ajax(url,{
         method: 'GET',
     }).done(function(result){
         alert($(result).text());
+        $("#result").html(result);
+    }).fail(function(error){
+        $("#result").html("An error has occurred !");
+    });
+}
+
+function searchAll(){
+    var url = "https://info2180-lab7-glawton.c9users.io/world.php?all=true";
+     $.ajax(url,{
+        method: 'GET',
+    }).done(function(result){
+        //alert($(result).text());
         $("#result").html(result);
     }).fail(function(error){
         $("#result").html("An error has occurred !");
--- world.php
+++ world.php
@@ -1,13 +1,27 @@
 <?php
+
+//To ensure the browser uses the correct encoding
+header("Content-Type: text/html; charset=ISO-8859-1");
+
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 $country = $_GET['country'];
+$all = $_Get['all'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-
+if($all =='true'){
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) {
+      echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
+else if($country !== ""){
     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo '<ul>';
@@ -15,3 +29,4 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
       echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
     }
     echo '</ul>';
+}
